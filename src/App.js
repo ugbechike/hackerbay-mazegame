b53afa4545b223d8fb2fcfa347319b745002f3a4
chycocee1 <engr.ugbechike@gmail.com>
2019-06-05 15:47:15 +0100

    modified app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,43 @@
 import React, {Component} from 'react';
 import './App.css';
 import BeginGame from './components/enterGame';
+import GameScore from './components/gameScore/gameScore';
+import GameContainer from './components/gameCells/gameCells';
+
+let createReactClass = require('create-react-class');
+let items = []
+let no_of_moves;
+const max_no_of_moves = 64;
+let mario_jump;
+let max_mashroom;
+
+
+
 
 class App extends Component {
+  constructor(props){
+		super(props);
+		let width = prompt("Enter width of game: ", "e.g. 10,20,30");
+		let height = prompt("Enter height of  game: ", "e.g. 10,20,30");
+		if(height == null || width == null || isNaN(width) === true || isNaN(height) === true){
+			height = 10
+      width =  10
+		}
+		let matrix_size = height * width
+		mario_jump = width
+		this.state = {
+			matrix_size:matrix_size,
+			width:width,
+      height:height,
+		}
+  }
 
   render(){
     return (
       <div className="App">
         <BeginGame />
-        welcome to my maze game
+        <GameContainer matrix={this.state.matrix_size}/>
+        <GameScore />
       </div>
     )
   }
