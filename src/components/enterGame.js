01de0bb3eb1cca3b946daccbbaf1085ac546ac94
chycocee1 <engr.ugbechike@gmail.com>
2019-06-05 15:01:34 +0100

    game information

---
+++ src/components/enterGame.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const BeginGame = (props) => {
+    return (
+        <div className="getting_started">
+			<h2>Maze Game</h2>
+			<p>You need to eat all the mashrooms in 64 steps. By Default maze size is 10 x 10, you can change it by reloading or entering the height and width in prompt.</p>
+		</div>
+    )
+}
+
+export default BeginGame;
