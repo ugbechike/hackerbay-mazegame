8885a2eac34b07518cfb433bda49959da9255f9a
chycocee1 <engr.ugbechike@gmail.com>
2019-06-05 15:11:00 +0100

    modified app component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
 import './App.css';
+import BeginGame from './components/enterGame';
 
 class App extends Component {
 
   render(){
     return (
       <div className="App">
+        <BeginGame />
         welcome to my maze game
       </div>
     )
